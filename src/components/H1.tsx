8f5534569a8e8ce2a96e049a7f034563437eecad
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-15 12:07:00 +0200

    Upd headers

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,7 +1,15 @@
 export default function H1({ children }: { children?: string }) {
   return (
     <>
-      <h1 className={children ? '' : 'hidden'} >{children}</h1>
+      <h1
+        className={
+          children
+            ? "text-center text-[3.5rem] font-black leading-[3.5rem] tracking-[-1px] md:tracking-[-1.5px] lg:text-[4.75rem] lg:leading-[4.75rem]"
+            : "hidden"
+        }
+      >
+        {children}
+      </h1>
     </>
   );
 }
--- src/components/H3.tsx
+++ src/components/H3.tsx
@@ -1,7 +1,15 @@
 export default function H3({ children }: { children?: string }) {
   return (
     <>
-      <h3 className={children ? '' : 'hidden'} >{children}</h3>
+      <h3
+        className={
+          children
+            ? "text-[1.5rem] font-bold lg:text-[2rem] lg:leading-[2.5rem]"
+            : "hidden"
+        }
+      >
+        {children}
+      </h3>
     </>
   );
 }
