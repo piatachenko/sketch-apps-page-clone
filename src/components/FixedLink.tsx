b890c555a76a8dc12f39ac3e12234995eb6f77e4
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-17 12:02:00 +0200

    Style fixed link

--- src/components/FixedLink.tsx
+++ src/components/FixedLink.tsx
@@ -4,7 +4,7 @@ export default function FixedLink() {
   return (
     <Link
       href={"https://www.sketch.com/blog/tiny-gems-sketch-improvements-2023/"}
-      className="block text-center arrow"
+      className="arrow block border-b border-black/10 py-[.6rem] text-center text-[.875rem] font-medium leading-[1.3125rem] tracking-[-.05px] after:translate-y-2 after:scale-[.85] hover:underline"
     >
       Tiny Gems: Some dazzling improvements to Sketch you might have missed
     </Link>
