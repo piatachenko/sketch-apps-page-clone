4173d87e190878a18972c3a69c7878891b4d3e31
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-19 12:02:00 +0200

    Style navlink

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -8,7 +8,12 @@ interface NavLinkProps {
 export default function NavLink({ text, link }: NavLinkProps) {
   return (
     <>
-      <Link href={link}>{text}</Link>
+      <Link
+        href={link}
+        className="p-4 font-medium transition-colors duration-200 hover:text-black/[.48]"
+      >
+        {text}
+      </Link>
     </>
   );
 }
