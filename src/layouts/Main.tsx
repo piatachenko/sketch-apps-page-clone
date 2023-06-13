9f16e698a05c12cdbd644f3636cd6f01574caf6b
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-06-13 12:00:00 +0200

    Remove fixed link

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,12 +1,10 @@
 import { type ReactNode } from "react";
-import FixedLink from "~/components/FixedLink";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function Main({ children }: { children: ReactNode }) {
   return (
     <>
-      <FixedLink/>
       <div className="mx-auto max-w-[75rem]">
         <Navbar />
         <main>{children}</main>
