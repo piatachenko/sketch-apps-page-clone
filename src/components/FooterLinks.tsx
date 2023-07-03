fa6f3ebcd96173bb26f1aa774c12403bb7fad4be
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-03 12:00:00 +0200

    Add footer links data

---
+++ src/components/FooterLinks.tsx
@@ -0,0 +1,51 @@
+const footerLinks = [
+  [
+    ["Product"],
+    ["What's New", "https://sketch.com/updates/"],
+    ["Design", "https://sketch.com/design/"],
+    ["Collaboration", "https://sketch.com/collaborate/"],
+    ["Prototyping", "https://sketch.com/prototype/"],
+    ["Developer Handoff", "https://sketch.com/handoff/"],
+    ["Apps", "https://sketch.com/apps/"],
+    ["Extensions", "https://sketch.com/extensions/"],
+    ["All Features", "https://sketch.com/features/"],
+    ["Pricing", "https://sketch.com/pricing/"],
+  ],
+  [
+    ["Why Sketch"],
+    ["Switch to Sketch", "https://sketch.com/switch-to-sketch/"],
+    ["Sketch vs. Figma", "https://sketch.com/vs/figma/"],
+    ["Sketch vs. Adobe XD", "https://sketch.com/vs/xd/"],
+    ["Free for Education", "https://sketch.com/education/"],
+  ],
+  [
+    ["Learn"],
+    ["Blog", "https://sketch.com/blog/"],
+    ["Course: Sketch 101", "https://sketch.com/courses/sketch-101/"],
+    ["Course: Sketch 102", "https://sketch.com/courses/sketch-102/", "new"],
+    ["Documentation", "https://sketch.com/docs/"],
+    ["Forum & Events", "https://sketch.com/community/", "new"],
+    ["Newsletter", "https://sketch.com/newsletter/"],
+    ["Developer Platform", "https://developer.sketch.com/"],
+  ],
+  [
+    ["Support"],
+    ["Sketch Help", "https://help.sketch.com/"],
+    ["Contact Us", "https://sketch.com/support/contact/"],
+    ["Manage License", "https://sketch.com/license/"],
+    ["Service Status", "https://status.sketch.com/"],
+  ],
+  [
+    ["Company"],
+    ["About Us", "https://sketch.com/about-us/"],
+    ["Careers", "https://sketch.com/about-us/#careers-at-sketch"],
+    ["Terms & Policies", "https://sketch.com/legal/"],
+    ["Cookie Preferences", "https://sketch.com/apps/#"],
+  ],
+];
+
+export default function Footer() {
+  return (
+    <></>
+  );
+}
