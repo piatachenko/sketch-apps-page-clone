255e7711f9410893b4ccf4e9d3c93f5b8a43ce24
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-04 12:03:00 +0200

    Add h5

---
+++ src/components/H5.tsx
@@ -0,0 +1,9 @@
+export default function H5({ children }: { children: string }) {
+  return (
+    <>
+      <h5 className="mb-1 text-[1.0625rem] font-semibold leading-6 text-[#212123]">
+        {children}
+      </h5>
+    </>
+  );
+}
