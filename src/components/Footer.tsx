5c5e934f0e76dafdb78cf2f000f575aecd165804
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-04 12:02:00 +0200

    Upd footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,12 @@
+import FooterLinks from "./FooterLinks";
+
 export default function Footer() {
   return (
-    <></>
+    <>
+      <footer className="pt-2">
+        <FooterLinks />
+        <div></div>
+      </footer>
+    </>
   )
 }
