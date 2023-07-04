f71fa63e64ce21cec748278dab102260b176210e
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-04 12:01:00 +0200

    Upd h6

--- src/components/H6.tsx
+++ src/components/H6.tsx
@@ -1,7 +1,9 @@
 export default function H6({ children }: { children: string }) {
   return (
     <>
-      <h6 className="">{children}</h6>
+      <h6 className="mb-2 font-semibold leading-6 text-[#212123]">
+        {children}
+      </h6>
     </>
   );
 }
