35c3696affe9eb5144002b3083bc97b173e54ce3
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-05 12:00:00 +0200

    Upd nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,50 +10,50 @@ const navLinks = [
       stripe: "/stripe-product.png",
       "": {
         Design: {
-          link: "https://www.sketch.com/design/",
+          link: "https://sketch.com/design/",
           stripe: "/stripe-design.png",
         },
         Collaboration: {
-          link: "https://www.sketch.com/collaborate/",
+          link: "https://sketch.com/collaborate/",
           stripe: "/stripe-collaboration.png",
         },
         Prototyping: {
-          link: "https://www.sketch.com/prototype/",
+          link: "https://sketch.com/prototype/",
           stripe: "/stripe-prototyping.png",
         },
         "Developer Handoff": {
-          link: "https://www.sketch.com/handoff/",
+          link: "https://sketch.com/handoff/",
           stripe: "/stripe-developer-handoff.png",
         },
         Extensions: {
-          link: "https://www.sketch.com/extensions/",
+          link: "https://sketch.com/extensions/",
           stripe: "/stripe-extensions.png",
         },
         "All Features": {
-          link: "https://www.sketch.com/features/",
+          link: "https://sketch.com/features/",
           stripe: "/stripe-features.png",
         },
       },
       "What's new": {
         "Experimental features menu": {
-          link: "https://www.sketch.com/updates/#experimental-features-menu",
+          link: "https://sketch.com/updates/#experimental-features-menu",
           date: "18 APR",
         },
         "Resolve Annotations": {
-          link: "https://www.sketch.com/updates/#resolve-annotations",
+          link: "https://sketch.com/updates/#resolve-annotations",
           date: "18 APR",
         },
         "Version 96 is available to download": {
-          link: "https://www.sketch.com/updates/#version-96-is-available-to-download",
+          link: "https://sketch.com/updates/#version-96-is-available-to-download",
           date: "18 APR",
         },
         "Annotations in the Mac app": {
-          link: "https://www.sketch.com/updates/#annotations-in-the-mac-app",
+          link: "https://sketch.com/updates/#annotations-in-the-mac-app",
           date: "18 JAN",
         },
         more: {
           text: "See all updates",
-          link: "https://www.sketch.com/updates/",
+          link: "https://sketch.com/updates/",
         },
       },
     },
@@ -63,31 +63,31 @@ const navLinks = [
       stripe: "/stripe-learn.png",
       Courses: {
         "Sketch 101": {
-          link: "https://www.sketch.com/courses/sketch-101/",
+          link: "https://sketch.com/courses/sketch-101/",
           stripe: "/stripe-sketch-101.png",
         },
         "Sketch 102": {
-          link: "https://www.sketch.com/courses/sketch-102/",
+          link: "https://sketch.com/courses/sketch-102/",
           stripe: "/stripe-sketch-102.png",
         },
       },
       More: {
-        Blog: "https://www.sketch.com/blog/",
-        Documentation: "https://www.sketch.com/docs/",
+        Blog: "https://sketch.com/blog/",
+        Documentation: "https://sketch.com/docs/",
         "Community Forum": "https://forum.sketch.com/",
-        Newsletter: "https://www.sketch.com/newsletter/",
+        Newsletter: "https://sketch.com/newsletter/",
       },
     },
   },
-  { Apps: "https://www.sketch.com/apps/" },
-  { Pricing: "https://www.sketch.com/pricing/" },
+  { Apps: "https://sketch.com/apps/" },
+  { Pricing: "https://sketch.com/pricing/" },
   {
     "Why Sketch": {
       "": {
-        "Switch to Sketch": "https://www.sketch.com/switch-to-sketch/",
-        "Sketch vs. Figma": "https://www.sketch.com/vs/figma/",
-        "Sketch vs. Adobe XD": "https://www.sketch.com/vs/xd/",
-        "Free for Education": "https://www.sketch.com/education/",
+        "Switch to Sketch": "https://sketch.com/switch-to-sketch/",
+        "Sketch vs. Figma": "https://sketch.com/vs/figma/",
+        "Sketch vs. Adobe XD": "https://sketch.com/vs/xd/",
+        "Free for Education": "https://sketch.com/education/",
       },
     },
   },
@@ -95,7 +95,7 @@ const navLinks = [
     Support: {
       "": {
         "Sketch Help": "https://help.sketch.com/",
-        "Contact Us": "https://www.sketch.com/support/contact/",
+        "Contact Us": "https://sketch.com/support/contact/",
         "Service Status": "https://status.sketch.com/",
       },
     },
@@ -109,7 +109,7 @@ export default function Navbar() {
         <div className="mb-[5.85rem] flex items-center justify-between py-[1.55rem]">
           <div className="flex items-center gap-6">
             <Link
-              href={"https://www.sketch.com/"}
+              href={"https://sketch.com/"}
               className="logo aspect-square w-[32px]"
             />
             <div>
@@ -127,10 +127,10 @@ export default function Navbar() {
             </div>
           </div>
           <div className="space-x-4">
-            <NavLink text={"Sign In"} link={"https://www.sketch.com/signin/"} />
+            <NavLink text={"Sign In"} link={"https://sketch.com/signin/"} />
             <ActionLink
               text={"Get started"}
-              link={"https://www.sketch.com/signup/"}
+              link={"https://sketch.com/signup/"}
             />
           </div>
         </div>
