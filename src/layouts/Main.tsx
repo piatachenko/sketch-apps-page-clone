1761c32f374791e2e882eb882c0a3eac4cbc6e24
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-09 12:01:00 +0200

    Upd main layout px

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -5,10 +5,12 @@ import Navbar from "../components/Navbar";
 export default function Main({ children }: { children: ReactNode }) {
   return (
     <>
-      <div className="mx-auto max-w-[75rem] px-4 sm:px-6">
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+      <div className="px-4 sm:px-6">
+        <div className="mx-auto max-w-[75rem]">
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </div>
       </div>
     </>
   );
